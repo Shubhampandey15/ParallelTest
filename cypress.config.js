f8cacbbc8a1699835f151bb9a455aad20756f2d4
shubham pandey <shubham.pandey@reveleer.com>
2024-04-15 14:50:27 +0530

    dashobard

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   projectId: "2eaw87",
   e2e: {
+    baseurul:"http://localhost:3000",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
